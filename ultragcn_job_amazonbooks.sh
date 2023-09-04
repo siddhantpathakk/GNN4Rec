556b17c45f74124a635ba98827debee10864b373
Siddhant Pathak <siddhant005@e.ntu.edu.sg>
2023-09-04 10:50:29 +0800

    add ultragcn baseline logs

---
+++ ultragcn_job_amazonbooks.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --partition=SCSEGPU_UG 
+#SBATCH --qos=normal
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --mem=10G
+#SBATCH --job-name=ultragcn_amazonbooks_m1
+#SBATCH --output=./logs/%x_%j.out 
+#SBATCH --error=./logs/%x_%j.err
+
+module load anaconda
+source activate ultragcn
+python main.py --config_file ./config/ultragcn_amazonbooks_m1.ini
